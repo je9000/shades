086e3ad0695edad6e2835c97f462d97539ac20d7
je9000 <none@example.com>
2018-09-11 00:28:38 -0700

    Minus my bugs, poll works with utun.

--- shades/NetDriver/NetDriver.cpp
+++ shades/NetDriver/NetDriver.cpp
@@ -26,41 +26,19 @@ uint32_t NetDriver::get_mtu() {
 }
 
 void NetDriver::setup_socket_ready(int fd) {
-#ifdef NETDRIVER_KQUEUE
-    kq = kqueue();
-    if (!kq) throw std::system_error(errno, std::generic_category(), "kqueue");
-    memset(&kq_chlist, 0, sizeof(kq_chlist));
-    memset(&kq_evlist, 0, sizeof(kq_evlist));
-    EV_SET(&kq_chlist[0], fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
-#endif
-    
-#ifdef NETDRIVER_POLL
     memset(&poll_data, 0, sizeof(poll_data));
     poll_data[0].fd = fd;
     poll_data[0].events = POLLIN;
-#endif
 }
 
 bool NetDriver::socket_ready(int timeout_seconds) {
     int r;
 
-#ifdef NETDRIVER_KQUEUE
-RETRY_KQUEUE:
-    kq_timeout.tv_sec = timeout_seconds;
-    kq_timeout.tv_nsec = 0;
-    r = kevent(kq, kq_chlist, sizeof(kq_chlist) / sizeof(kq_chlist[0]), kq_evlist, sizeof(kq_evlist) / sizeof(kq_evlist[0]), &kq_timeout);
-    if (r < 0 && (errno == EINTR || errno == EAGAIN)) goto RETRY_KQUEUE;
-    if (r < 0) throw std::system_error(errno, std::generic_category(), "kevent");
-    if (r == 0) return false; // Timeout
-#endif
-    
-#ifdef NETDRIVER_POLL
 RETRY_POLL:
-    r = poll(poll_data, sizeof(poll_data) / sizeof(poll_data[0]), timeout_seconds);
+    r = poll(poll_data, sizeof(poll_data) / sizeof(poll_data[0]), timeout_seconds * 1000);
     if (r < 0 && (errno == EINTR || errno == EAGAIN)) goto RETRY_POLL;
     if (r < 0) throw std::system_error(errno, std::generic_category(), "poll");
     if (r == 0) return false; // Timeout
-#endif
 
     return true;
 }
